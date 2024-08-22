3556297ca2b265a4ef4ac909f5273aadfc2cb1b8
FabianUcode <u202214604@upc.edu.pe>
2024-08-21 21:10:16 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/medlearn/MedLearnApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.medlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedLearnApplication.class, args);
+    }
+
+}
