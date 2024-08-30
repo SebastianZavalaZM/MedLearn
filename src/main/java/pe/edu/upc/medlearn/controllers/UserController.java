1d10e8d5d1e0bb8f32dc2d91d558d6f288deb7ae
Fabrizio Hidalgo <academyfabrizio7@gmail.com>
2024-08-30 10:05:27 -0500

    user issue

--- src/main/java/pe/edu/upc/medlearn/controllers/UserController.java
+++ src/main/java/pe/edu/upc/medlearn/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.medlearn.dtos.UserDTO;
+import pe.edu.upc.medlearn.entities.Users;
 import pe.edu.upc.medlearn.servicesinterfaces.IUserService;
 
 import java.util.List;
@@ -19,6 +20,19 @@ public class UserController {
             ModelMapper m=new ModelMapper();
             return m.map(x,UserDTO.class);
         }).collect(Collectors.toList());
+    }
+    @PostMapping("/insertar")
+    public void insert(@RequestBody UserDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Users user = m.map(dto, Users.class);
+        uS.insert(user);
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO listId(@PathVariable("id") Integer id) {
+        ModelMapper m = new ModelMapper();
+        UserDTO dto = m.map(uS.listId(id), UserDTO.class);
+        return dto;
     }
 
 }
