226a243776762b3ce80b6719036ab4d06501063a
Fabrizio Hidalgo <academyfabrizio7@gmail.com>
2024-08-30 21:52:58 -0500

    realease complete

--- src/main/java/pe/edu/upc/medlearn/MedLearnApplication.java
+++ src/main/java/pe/edu/upc/medlearn/MedLearnApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-MedLearnApplication {
+public class MedLearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MedLearnApplication.class, args);
