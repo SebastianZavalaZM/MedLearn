7dfc789a2b29801f77121f67c4efeffd8fe95f37
Fabrizio Hidalgo <academyfabrizio7@gmail.com>
2024-09-01 16:16:26 -0500

    branch issue

--- src/main/java/pe/edu/upc/medlearn/servicesinterfaces/IIllnessService.java
+++ src/main/java/pe/edu/upc/medlearn/servicesinterfaces/IIllnessService.java
@@ -1,15 +1,14 @@
 package pe.edu.upc.medlearn.servicesinterfaces;
 import pe.edu.upc.medlearn.entities.Illness;
+import pe.edu.upc.medlearn.entities.Users;
+
 import java.util.List;
 
 public interface IIllnessService {
-    public List<Illness> list();
-    public void insert(Illness illness);
-    public void delete(Integer Id);
-    public Illness listId(int id);
+    public List<Users> list();
+    public void insert(Users user);
+    public Users listId(int id);
+    public List<Users> search(String name);
 
-    public  void update(Illness a);
-
-    public void  delete(int id);
 
 }
