fd9206b4e8493122672bdce17d5205fb5f513d51
maria tapia hurtado <138945833+anamariata@users.noreply.github.com>
2024-09-02 18:23:40 -0500

    Add files via upload
    food and exercises DTOs

---
+++ src/main/java/pe/edu/upc/medlearn/dtos/ExercisesDTO.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.medlearn.dtos;
+
+public class ExercisesDTO {
+    private int idExercises;
+    private String nameExercises;
+    private int setsExercises;
+
+    public int getIdExercises() {
+        return idExercises;
+    }
+
+    public void setIdExercises(int idExercises) {
+        this.idExercises = idExercises;
+    }
+
+    public String getNameExercises() {
+        return nameExercises;
+    }
+
+    public void setNameExercises(String nameExercises) {
+        this.nameExercises = nameExercises;
+    }
+
+    public int getSetsExercises() {
+        return setsExercises;
+    }
+
+    public void setSetsExercises(int setsExercises) {
+        this.setsExercises = setsExercises;
+    }
+}
