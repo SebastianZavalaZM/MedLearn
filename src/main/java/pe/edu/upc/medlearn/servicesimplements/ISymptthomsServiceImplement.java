c928861542411b33dc5a31be517768b377f4177d
Fabian_PC <91865476+fcodex03@users.noreply.github.com>
2024-09-05 19:16:13 -0500

    arreglado el service implement

--- src/main/java/pe/edu/upc/medlearn/servicesimplements/ISymptthomsServiceImplement.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.medlearn.servicesimplements;
-import org.springframework.stereotype.Service;
-import pe.edu.upc.medlearn.servicesinterfaces.ISympthomsService;
-
-
-@Service
-public interface ISymptthomsServiceImplement extends ISympthomsService {
-
-}
---
+++ src/main/java/pe/edu/upc/medlearn/servicesimplements/SympthomsServiceImplement.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.medlearn.servicesimplements;
+
+public class SympthomsServiceImplement {
+}
